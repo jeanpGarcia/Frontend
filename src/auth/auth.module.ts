cf0cf4d024ddcf62a4ccd2ae5b7eef7924d0e5b5
AdrianC1530 <danilocarrillo1515@gmail.com>
2025-04-28 22:06:09 -0500

    Modificacion jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -16,4 +16,4 @@ import { JwtModule } from '@nestjs/jwt';
   providers: [AuthService],
 })
 export class AuthModule {}
-//Creacion de modulo para autenticar usuarios usando JWT para encriptar los mismos 
+//Creacion de modulo para autenticar usuarios usando JWT 
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // 👈 Importa esto
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  // 👇 Configuración de Swagger
+
   const config = new DocumentBuilder()
     .setTitle('Login API')
     .setDescription('API para registro y login de usuarios')
@@ -15,7 +15,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  // 👆 Esto muestra la documentación en http://localhost:3000/api
+  
 
   await app.listen(3000);
 }
